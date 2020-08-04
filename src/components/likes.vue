741264443f20992ed4aeb4569acaac863938f88c
hareeshcs33 <hareeshcs33@gmail.com>
2020-08-04 07:57:28 +0530

    minor change

--- src/components/likes.vue
+++ src/components/likes.vue
@@ -1,12 +1,11 @@
 <template>
     <div>
-        Likes
-        <div>
+        <h3>Likes</h3>
+        <!-- <div>
             <button v-popover:foo>Toggle popover</button>
-
             <popover name="foo" :width="120">
                 <a href="#">Well, hello there!</a>
             </popover>
-        </div>
+        </div> -->
     </div>
 </template>
